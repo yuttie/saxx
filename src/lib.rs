18fd3d97aea793066932272aece495c43f0cb3cf
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-11-08 19:30:21 +0900

    Insert "C"

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate libc;
 use libc::c_int;
 
 
-extern {
+extern "C" {
     pub fn esaxx_c8i8  (t: *const  u8, sa: *const  i8, l: *const  i8, r: *const  i8, d: *const  i8, n:  i8, k:  i8, m: *mut  i8) -> c_int;
     pub fn esaxx_c8i16 (t: *const  u8, sa: *const i16, l: *const i16, r: *const i16, d: *const i16, n: i16, k: i16, m: *mut i16) -> c_int;
     pub fn esaxx_c8i32 (t: *const  u8, sa: *const i32, l: *const i32, r: *const i32, d: *const i32, n: i32, k: i32, m: *mut i32) -> c_int;
