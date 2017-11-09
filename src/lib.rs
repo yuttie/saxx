30aa877cca7d121ecdd83919b25dc6b31798740c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-11-10 00:57:27 +0900

    Add a high-level wrapper function around esaxx_*()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate libc;
 
 
+use std::result;
 use libc::c_int;
 
 
@@ -21,4 +22,58 @@ extern "C" {
     pub fn esaxx_c64i16(t: *const u64, sa: *mut i16, l: *mut i16, r: *mut i16, d: *mut i16, n: i16, k: i16, m: *mut i16) -> c_int;
     pub fn esaxx_c64i32(t: *const u64, sa: *mut i32, l: *mut i32, r: *mut i32, d: *mut i32, n: i32, k: i32, m: *mut i32) -> c_int;
     pub fn esaxx_c64i64(t: *const u64, sa: *mut i64, l: *mut i64, r: *mut i64, d: *mut i64, n: i64, k: i64, m: *mut i64) -> c_int;
+}
+
+
+pub type Error = i8;
+
+pub type Result<T> = result::Result<T, Error>;
+
+pub struct Esa<T> {
+    pub sa: Vec<T>,
+    pub l: Vec<T>,
+    pub r: Vec<T>,
+    pub d: Vec<T>,
+    pub m: T,
+}
+
+
+pub trait Esaxx<Index> {
+    fn esaxx(t: &Self) -> Result<Esa<Index>>;
+}
+
+
+impl Esaxx<i8> for [u8] {
+    fn esaxx(t: &[u8]) -> Result<Esa<i8>> {
+        let length = t.len();
+        let max_char: u8 = *t.iter().max().unwrap_or(&0);
+        assert!(length <= i8::max_value() as usize);
+        assert!(max_char <= i8::max_value() as u8);
+
+        let n: i8 = length as i8;
+        let k: i8 = max_char as i8;
+        let mut sa: Vec<i8> = Vec::new();
+        let mut  l: Vec<i8> = Vec::new();
+        let mut  r: Vec<i8> = Vec::new();
+        let mut  d: Vec<i8> = Vec::new();
+        let mut  m: i8 = 0;
+
+        let ret = unsafe {
+            esaxx_c8i8(t.as_ptr(),
+                       sa.as_mut_ptr(),
+                       l.as_mut_ptr(),
+                       r.as_mut_ptr(),
+                       d.as_mut_ptr(),
+                       n as i8,
+                       k as i8,
+                       &mut m as *mut i8) as Error
+        };
+
+        if ret == 0 {
+            Ok(Esa { sa, l, r, d, m })
+        }
+        else {
+            Err(ret)
+        }
+    }
 }
